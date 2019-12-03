ff892693c91f61b0ad0ce192c2a782d01a54930f
kmdgs <kmdgs@qq.com>
2019-12-03 09:14:49 +0800

    代码跳转

--- pages/view/view.js
+++ pages/view/view.js
@@ -6,44 +6,27 @@ Page({
    */
   data: {
     id: 0,
-    name: '',
-    age: 0
+    title: ''
   },
 
+  backHome(){
+       wx.navigateBack({
+         delta:1
+       })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
     this.setData({
       id: options.id,
-      age: options.age,
-      name: options.name
+      title: options.title
     })
     console.log(options);
     console.log(options.id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
+  
   /**
    * 生命周期函数--监听页面卸载
    */
@@ -57,25 +40,4 @@ Page({
      title:'跳回首页了'
    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
 })
