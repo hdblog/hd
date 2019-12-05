bc2c501bce5fdd7d71a961bbc7885ab336053bf5
kmdgs <kmdgs@qq.com>
2019-12-05 19:41:18 +0800

    砍价拼团 组件化

---
+++ pages/goods-details/childcomponents/h-kanjia/h-kanjia.js
@@ -0,0 +1,26 @@
+// pages/goods-details/childcomponents/h-kanjia/h-kanjia.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    curGoodsKanjia:{
+      type:Array,
+      value:[]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/goods-details/childcomponents/h-pintuan/h-pintuan.js
@@ -0,0 +1,26 @@
+// pages/goods-details/childcomponents/h-pintuan.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    pingtuanSet:{
+      type:Array,
+      value:{}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
